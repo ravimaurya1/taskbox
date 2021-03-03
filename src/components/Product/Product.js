2094e6d14071c200b53ef238f88fcf281ed2a7a9
RAVI MAURYA <ravimaurya@QMAC0239.local>
2021-03-03 11:17:05 +0530

    Style changed

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from "react";
 import { gql } from "@apollo/client";
 import Productinfo from "../Product_Info/Productinfo";
 import Magnifier from "../Magnifier/Magnifier";
+import "./Product.css";
 
 const Product = () => {
   const [pid, setPid] = useState(401949);
   return (
     <div className="Product">
-      <div className="Product Info">
-        <div className="Product Image">
+      <div className="ProductInfo">
+        <div className="ProductImage">
           <Magnifier pid={pid} />
         </div>
         <div className="Info">
--- src/components/Product_Info/Productinfo.js
+++ src/components/Product_Info/Productinfo.js
@@ -1,27 +1,37 @@
 import React, { useEffect, useState } from "react";
 import { useQuery, gql } from "@apollo/client";
+import "./Productinfo.css";
 
 const PROD_INFO = gql`
   query prod_info($id: ID!) {
     info(id: $id) {
       title
+      condition
+      price
+      availability
+      offerPrice
+      name
     }
   }
 `;
-
 const Productinfo = (props) => {
   const { loading, error, data } = useQuery(PROD_INFO, {
     variables: { id: props.pid },
   });
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error!</div>;
-  {
-    console.log(data);
-  }
+  const { name, condition, offerPrice, price, title } = data.info;
   return (
     <div className="Productinfo">
       <div className="header">
-        <h1></h1>
+        <h2>{name}</h2>
+        <h3>Condition. {condition}</h3>
+        <hr />
+      </div>
+      <div className="BuyNow">
+        <h2>Rs. {offerPrice}</h2>
+        <h4>Est original Rs.{price}</h4>
+        <button className="BuyNowButton">BUY NOW</button>
       </div>
     </div>
   );
