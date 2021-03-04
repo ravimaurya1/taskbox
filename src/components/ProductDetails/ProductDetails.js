fa5012e7cd3bb9a90d177e5ae1485d32b4ab0d3c
RAVI MAURYA <ravimaurya@QMAC0239.local>
2021-03-04 10:51:19 +0530

    New Changes are made

--- src/components/ProductDetails/ProductDetails.js
+++ src/components/ProductDetails/ProductDetails.js
@@ -9,6 +9,10 @@ const QUEST = gql`
         heading
         description
       }
+      headingAndDescription {
+        heading
+        description
+      }
     }
   }
 `;
@@ -20,17 +24,29 @@ const ProductDetails = (props) => {
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error!</div>;
   const question = data.info.question_ans;
+  const warratny = data.info.headingAndDescription;
   return (
     <div className="Details">
       <div className="question">
-        <h2>QUESTIONS ABOUT THIS PRODUCT</h2>
         <div className="question1">
-          <h3>{question[0].heading}</h3>
-          <p>{question[0].description}</p>
+          <h3>{warratny[0].heading}</h3>
+          <p>{warratny[0].description}</p>
         </div>
         <div className="question2">
-          <h3>{question[1].heading}</h3>
-          <p>{question[1].description}</p>
+          <h3>{warratny[1].heading}</h3>
+          <p>{warratny[1].description}</p>
+        </div>
+        <hr />
+        <div className="question">
+          <h2>QUESTIONS ABOUT THIS PRODUCT</h2>
+          <div className="question1">
+            <h3>{question[0].heading}</h3>
+            <p>{question[0].description}</p>
+          </div>
+          <div className="question2">
+            <h3>{question[1].heading}</h3>
+            <p>{question[1].description}</p>
+          </div>
         </div>
       </div>
     </div>
